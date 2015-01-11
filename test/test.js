093cb7338c5239df960b55f44325b2d0ed632293
adius <mail@adriansieber.com>
2015-01-11 22:28:00 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,35 @@
 var nodeScad = require('../index.js'),
-	path = require('path')
+	path = require('path'),
+	assert = require('assert')
 
 
-nodeScad.renderFile(
+nodeScad.render(
 	{
 		inputFile: path.join(__dirname, 'cone.scad')
 	},
-	function (error, data) {
+	function (error, bufferData) {
 		if (error)
 			console.error(error.message)
 		else
-			console.log(data.toString('utf8'))
+			assert.equal(
+				bufferData.toString('utf8').length,
+				603433,
+				'Stl file has wrong size'
+			)
+	}
+)
+
+nodeScad.renderFile(
+	path.join(__dirname, 'cone.scad'),
+	{},
+	function (error, bufferData) {
+		if (error)
+			console.error(error.message)
+		else
+			assert.equal(
+				bufferData.toString('utf8').length,
+				60343,
+				'Stl file has wrong size'
+			)
 	}
 )
